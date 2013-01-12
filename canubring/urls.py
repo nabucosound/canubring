9a6300e329797fe0093783d733681b6056407970
Hector Garcia <hector@nomadblue.com>
2013-01-12 18:40:47 -0300

    Added my profile template

--- canubring/urls.py
+++ canubring/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(regex=r'^$', view='website.views.home', name='home', kwargs = {'template': 'search.html',}),
+    url(r'^my/', include('website.urls')),
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^facebook/', include('django_facebook.urls')),
     # url(r'^auth/', include('django_facebook.auth_urls')),
--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ def home(request, template):
     ctxt = dict()
     return render(request, template, ctxt)
 
+def profile(request, template):
+    ctxt = dict()
+    return render(request, template, ctxt)
+
