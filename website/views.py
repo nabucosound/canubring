faeb99ee0c6743b3b27fbb7eed9ae7202194fc64
Hector Garcia <hector@nomadblue.com>
2013-03-01 10:17:52 -0300

    Made some webite views login_required

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
 
 from trips.models import Trip
 
@@ -13,27 +14,31 @@ def home(request, template):
     ctxt['trip_results'] = Trip.objects.order_by('-creation_dt')
     return render(request, template, ctxt)
 
+@login_required
 def profile(request, template):
     ctxt = dict()
     ctxt['profile'] = request.user.userprofile
     if request.session.pop('show_signup_sys_msg', False):
         ctxt['show_signup_sys_msg'] = True
-
     return render(request, template, ctxt)
 
+@login_required
 def trips(request, template):
     ctxt = dict()
     return render(request, template, ctxt)
 
+@login_required
 def new_trip(request, template):
     ctxt = dict()
     return render(request, template, ctxt)
 
+@login_required
 def cargos(request, template):
     ctxt = dict()
     ctxt['my_cargos'] = True
     return render(request, template, ctxt)
 
+@login_required
 def evaluations(request, template):
     ctxt = dict()
     return render(request, template, ctxt)
