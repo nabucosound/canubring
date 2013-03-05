093e319acafd054c70c53370d761cf18fa8753c9
Hector Garcia <hector@nomadblue.com>
2013-03-04 23:09:14 -0300

    Finished search and proper pagination

--- search/views.py
+++ search/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.db.models import Q
 
@@ -12,17 +14,30 @@ def search(request, template):
     departure_complex_lookup = None
     departure = request.GET.get('departure', None)
     if departure:
-        departure_complex_lookup = (Q(dep_city__name__icontains=departure) | Q(dep_country__name__icontains=departure))
+        dep_city = departure.split(',')[0].strip()
+        dep_country = departure.split(',')[-1].strip()
+        departure_complex_lookup = (Q(dep_city__name__iexact=dep_city) | Q(dep_country__name__iexact=dep_country))
 
     destination_complex_lookup = None
     destination = request.GET.get('destination', None)
     if destination:
-        destination_complex_lookup = (Q(dest_city__name__icontains=destination) | Q(dest_country__name__icontains=destination))
+        dest_city = destination.split(',')[0].strip()
+        dest_country = destination.split(',')[-1].strip()
+        destination_complex_lookup = (Q(dest_city__name__iexact=dest_city) | Q(dest_country__name__iexact=dest_country))
 
     if departure_complex_lookup:
         trip_results = trip_results.filter(departure_complex_lookup)
     if destination_complex_lookup:
         trip_results = trip_results.filter(destination_complex_lookup)
+
+    arrival = request.GET.get('arrival', None)
+    if arrival:
+        try:
+            date = datetime.datetime.strptime(arrival, "%m/%d/%Y")
+        except ValueError:
+            pass
+        else:
+            trip_results = trip_results.filter(destination_dt__year=date.year, destination_dt__month=date.month, destination_dt__day=date.day)
 
     ctxt['trip_results'] = listing(request, trip_results)
     return render(request, template, ctxt)
