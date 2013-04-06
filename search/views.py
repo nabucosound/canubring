93ba9badeadebdd0afc14e664aee1558f884a1c6
Hector Garcia <hector@nomadblue.com>
2013-04-06 20:03:56 -0300

    Search filtering, still not changing dinamically

--- search/views.py
+++ search/views.py
@@ -11,34 +11,35 @@ def search(request, template):
     ctxt = dict()
     now = datetime.datetime.now()
     trip_results = Trip.objects.filter(departure_dt__gte=now).order_by('departure_dt')
-    active_countries = Country.objects.filter(Q(dep_country_trips__in=trip_results) | Q(dest_country_trips__in=trip_results)).distinct().order_by('name')
-    active_cities = City.objects.filter(Q(dep_city_trips__in=trip_results) | Q(dest_city_trips__in=trip_results)).distinct().order_by('name')
-    ctxt['active_countries'] = active_countries
-    ctxt['active_cities'] = active_cities
+
+    active_dep_countries = Country.objects.filter(dep_country_trips__in=trip_results).distinct().order_by('name')
+    active_dest_countries = Country.objects.filter(dest_country_trips__in=trip_results).distinct().order_by('name')
+    ctxt['active_dep_countries'] = active_dep_countries
+    ctxt['active_dest_countries'] = active_dest_countries
 
     departure_country_complex_lookup = None
     departure_country = request.GET.get('departure_country', None)
     if departure_country:
-        dep_country = departure_country.split(',')[0].strip()
-        departure_country_complex_lookup = (Q(dep_country__id=dep_country))
+        ctxt['departure_country'] = int(departure_country)
+        departure_country_complex_lookup = (Q(dep_country__id=departure_country))
 
     destination_country_complex_lookup = None
     destination_country = request.GET.get('destination_country', None)
     if destination_country:
-        dest_country = destination_country.split(',')[0].strip()
-        destination_country_complex_lookup = (Q(dest_country__id=dest_country))
+        ctxt['destination_country'] = int(destination_country)
+        destination_country_complex_lookup = (Q(dest_country__id=destination_country))
 
     departure_city_complex_lookup = None
     departure_city = request.GET.get('departure_city', None)
     if departure_city:
-        dep_city = departure_city.split(',')[0].strip()
-        departure_city_complex_lookup = (Q(dep_city__id=dep_city))
+        ctxt['departure_city'] = int(departure_city)
+        departure_city_complex_lookup = (Q(dep_city__id=departure_city))
 
     destination_city_complex_lookup = None
     destination_city = request.GET.get('destination_city', None)
     if destination_city:
-        dest_city = destination_city.split(',')[0].strip()
-        destination_city_complex_lookup = (Q(dest_city__id=dest_city))
+        ctxt['destination_city'] = int(destination_city)
+        destination_city_complex_lookup = (Q(dest_city__id=destination_city))
 
     if departure_country_complex_lookup:
         trip_results = trip_results.filter(departure_country_complex_lookup)
@@ -49,14 +50,10 @@ def search(request, template):
     if destination_city_complex_lookup:
         trip_results = trip_results.filter(destination_city_complex_lookup)
 
-    # arrival = request.GET.get('arrival', None)
-    # if arrival:
-    #     try:
-    #         date = datetime.datetime.strptime(arrival, "%m/%d/%Y")
-    #     except ValueError:
-    #         pass
-    #     else:
-    #         trip_results = trip_results.filter(destination_dt__year=date.year, destination_dt__month=date.month, destination_dt__day=date.day)
+    active_dep_cities = City.objects.filter(dep_city_trips__in=trip_results).distinct().order_by('name')
+    active_dest_cities = City.objects.filter(dest_city_trips__in=trip_results).distinct().order_by('name')
+    ctxt['active_dep_cities'] = active_dep_cities
+    ctxt['active_dest_cities'] = active_dest_cities
 
     trips = listing(request, trip_results)
     ctxt['trips'] = trips
